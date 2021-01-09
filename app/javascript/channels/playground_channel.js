80b183b94b8e70e89ed0af5b0288c65126eee66e
Ch1g <it.chigiani@yahoo.com>
2021-01-09 05:50:46 +0300

    Real-time(actioncable) chat feature added and some css things

--- app/javascript/channels/playground_channel.js
+++ app/javascript/channels/playground_channel.js
@@ -1,18 +1,30 @@
 import consumer from "./consumer"
 
-document.addEventListener('turbolinks:load', () => {
+consumer.subscriptions.create({ channel: "PlaygroundChannel" }, {
+  connected() {
+  	console.log('Connected to playground channel successfully. /~/~/~')
+  },
 
-	consumer.subscriptions.create({ channel: "PlaygroundChannel" }, {
-	  connected() {
-	  	console.log('Connected to playground channel successfully. /~/~/~')
-	  },
+  disconnected() {
+  	console.log('Disconnected from playground channel successfully. /~/~/~')
+  },
 
-	  disconnected() {
-	  	console.log('Disconnected from playground channel successfully. /~/~/~')
-	  },
+  received(data) {
+  	console.log(data)
 
-	  received(data) {
-	  	console.log(data)
-	  }
-	});
-})
+    const element = document.getElementById('user-id');
+    const user_id = Number(element.getAttribute('data-user-id'))
+
+    let html;
+
+    if (user_id === data.message.user_id) {
+      html = data.mine
+      document.getElementById('message-box').value = '';
+    } else {
+      html = data.theirs
+    }
+
+  	const messageContainer = document.getElementById('messages')
+  	messageContainer.innerHTML = messageContainer.innerHTML + html
+  }
+});
---
+++ app/jobs/send_message_job.rb
@@ -0,0 +1,16 @@
+class SendMessageJob < ApplicationJob
+  queue_as :default
+
+  def perform(message)
+    mine = ApplicationController.render(
+      partial: 'messages/mine',
+      locals: { message: message })
+
+    theirs = ApplicationController.render(
+      partial: 'messages/theirs',
+      locals: { message: message })
+
+
+    ActionCable.server.broadcast "playground_channel", mine: mine, theirs: theirs, message: message
+  end
+end
