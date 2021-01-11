4c24f9545cfa4e7b43be194fc1b0276c1baf32ff
Ch1g <it.chigiani@yahoo.com>
2021-01-11 03:28:12 +0300

    css modifying (shadows, slider-hide, making full-width)

--- app/javascript/channels/playground_channel.js
+++ app/javascript/channels/playground_channel.js
@@ -1,30 +1,34 @@
-import consumer from "./consumer"
+import consumer from "./consumer";
 
-consumer.subscriptions.create({ channel: "PlaygroundChannel" }, {
-  connected() {
-  	console.log('Connected to playground channel successfully. /~/~/~')
-  },
+consumer.subscriptions.create(
+  { channel: "PlaygroundChannel" },
+  {
+    connected() {
+      console.log("Connected to playground channel successfully. /~/~/~");
+    },
 
-  disconnected() {
-  	console.log('Disconnected from playground channel successfully. /~/~/~')
-  },
+    disconnected() {
+      console.log("Disconnected from playground channel successfully. /~/~/~");
+    },
 
-  received(data) {
-  	console.log(data)
+    received(data) {
+      console.log(data);
 
-    const element = document.getElementById('user-id');
-    const user_id = Number(element.getAttribute('data-user-id'))
+      const element = document.getElementById("user-id");
+      const user_id = Number(element.getAttribute("data-user-id"));
 
-    let html;
+      let html;
 
-    if (user_id === data.message.user_id) {
-      html = data.mine
-      document.getElementById('message-box').value = '';
-    } else {
-      html = data.theirs
-    }
+      if (user_id === data.message.user_id) {
+        html = data.mine;
+        document.getElementById("message-box").value = "";
+      } else {
+        html = data.theirs;
+      }
 
-  	const messageContainer = document.getElementById('messages')
-  	messageContainer.innerHTML = messageContainer.innerHTML + html
+      const messageContainer = document.getElementById("messages");
+      messageContainer.innerHTML = messageContainer.innerHTML + html;
+      messageContainer.scrollTop = messageContainer.scrollHeight;
+    },
   }
-});
+);
