171883057e8b1324b679cc3d9dcb87cb61682bdb
Ch1g <it.chigiani@yahoo.com>
2021-01-17 09:51:48 +0300

    DRYing messages template-generation and rendering & refactored connection establishment

--- app/javascript/channels/lobby_channel.js
+++ app/javascript/channels/lobby_channel.js
@@ -1,44 +1,34 @@
-import consumer from "./consumer";
+import consumer from './consumer';
 
-// on page load
-window.addEventListener("load", (event) => {
+window.lobbyConnection = function (userId) {
   // variables declaring
-  const element = document.getElementById("user-id");
-  // check existance of element
-  if (element) {
-    const user_id = +element.getAttribute("data-user-id");
-    const messageContainer = document.getElementById("messages");
-    // scrolling chat to last message
-    messageContainer.scrollTop = messageContainer.scrollHeight;
+  const messageContainer = document.getElementById('messages');
+  // scrolling chat to last message
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+  // establishing connection
+  consumer.subscriptions.create(
+    { channel: 'LobbyChannel' },
+    {
+      // log connection status to console
+      connected() {
+        console.log('Connected to lobby channel successfully. /~/~/~');
+      },
 
-    consumer.subscriptions.create(
-      { channel: "LobbyChannel" },
-      {
-        // log connection status to console
-        connected() {
-          console.log("Connected to lobby channel successfully. /~/~/~");
-        },
+      disconnected() {
+        console.log('Disconnected from lobby channel successfully. /~/~/~');
+      },
+      //
 
-        disconnected() {
-          console.log("Disconnected from lobby channel successfully. /~/~/~");
-        },
-        //
-
-        // render broadcasted message to chat and scroll chat to last message
-        received(data) {
-          let html;
-
-          if (user_id === data.message.user_id) {
-            html = data.mine;
-            document.getElementById("message-box").value = "";
-          } else {
-            html = data.theirs;
-          }
-
-          messageContainer.innerHTML = messageContainer.innerHTML + html;
-          messageContainer.scrollTop = messageContainer.scrollHeight;
-        },
-      }
-    );
-  }
-});
+      // render broadcasted message to chat and scroll chat to last message
+      received(data) {
+        let html = new DOMParser().parseFromString(data.html, 'text/html').body.firstChild;
+        if (userId === data.message.user_id) {
+          html.className += ' me';
+          document.getElementById('message-box').value = '';
+        }
+        messageContainer.innerHTML += html.outerHTML;
+        messageContainer.scrollTop = messageContainer.scrollHeight;
+      },
+    }
+  );
+};
--- app/jobs/send_message_job.rb
+++ app/jobs/send_message_job.rb
@@ -2,15 +2,11 @@ class SendMessageJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    mine = ApplicationController.render(
-      partial: 'messages/mine',
-      locals: { message: message })
-
-    theirs = ApplicationController.render(
-      partial: 'messages/theirs',
+    html = ApplicationController.render(
+      partial: 'messages/message',
       locals: { message: message })
 
 
-    ActionCable.server.broadcast "lobby_channel", mine: mine, theirs: theirs, message: message
+    ActionCable.server.broadcast "lobby_channel", html: html, message: message
   end
 end
