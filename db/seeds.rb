2cc80e38476ffc47a61f3a61801979450b5be3d8
Ch1g <it.chigiani@yahoo.com>
2021-01-25 07:24:33 +0000

    game seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Crazy Button game seed:
+Game.create({title: 'Бешеная кнопка',
+            description: 'Эту кнопку нажимали столько раз, что даже представить трудно! Она сошла с ума и угрожает безопасности пользователей, её нужно изловить, пока дел не наворотила! Но помни, если решишься пойти на охоту, ты такой не один.',
+            default_render: '{"left": 50, "top": 50}',
+            asset_name: 'crazy_button'})
